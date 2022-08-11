bbfb73c467d755b10bd1c3d82454db9d61ae5475
Ayanava De <ayanavade01@gmail.com>
2022-08-11 12:34:58 +0530

    Adding a progress bar for the terminal ✨

--- apps/simulation/sim.cpp
+++ apps/simulation/sim.cpp
@@ -11,6 +11,38 @@
 
 static Context CTX;
 
+/**
+ * @brief Print the progress of a process. The function has been taken from
+ * https://gist.github.com/juliusikkala/946f505656ed3c35f6c2741f29f26080
+ * 
+ * @param p 
+ * @param total 
+ * @param width 
+ */
+void print_progress(int p, int total, int width = 80) {
+	std::string total_str = std::to_string(total);
+	std::string p_str = std::to_string(p);
+	int bar_width = width - total_str.size()*2 - 4;
+
+	std::cout << '\r';
+	if(bar_width > 1) {
+		int fill_width = bar_width * p / total;
+		std::cout << "[";
+		for(int i = 0; i < bar_width; ++i) {
+			char c = ' ';
+			if(i < fill_width || p == total) c = '=';
+			else if(i == fill_width) c = '>';
+
+			std::cout << c;
+		}
+		std::cout << "] ";
+	}
+	std::cout << std::setfill(' ') << std::setw(total_str.size())
+		<< "En " << p_str << "/" << total_str << std::flush;
+	if(p == total) std::cout << std::endl;
+}
+
+
 int main(int argc, char** argv) {
 	std::string filename;
 	if (argc > 1) filename = argv[1];
@@ -39,7 +71,8 @@ int main(int argc, char** argv) {
 		config.generate();
 
 		for (int ensemble = 0; ensemble < CTX.ENSEMBLE_SIZE; ensemble++) {
-			std::cout << "Ensemble member " << ensemble+1 << "\n";
+			// std::cout << "Ensemble member " << ensemble+1 << "\n";
+			print_progress(ensemble+1, CTX.ENSEMBLE_SIZE);
 			config.reinit();
 			open(&config, ensemble+1, parentDir);
 
