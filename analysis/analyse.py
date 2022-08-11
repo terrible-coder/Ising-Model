a312a8968691f468cbcfe7d6cbce5bc44f6c39c2
Ayanava De <ayanavade01@gmail.com>
2022-08-11 12:34:11 +0530

    Data analysis scripts
    Creating separate scripts for different graphs

---
+++ analysis/analyse.py
@@ -0,0 +1,87 @@
+import numpy as np
+from scipy import optimize as opt
+import pandas as pd
+import matplotlib.pyplot as plt
+
+begIdx = 5000
+endIdx = 15000
+dataIdxRange = [i for i in range(begIdx, endIdx)]
+EVERY = 50
+SKIP = 5
+
+def readData(temperature:float, size:int, ensemble:int, qty):
+	datafile = f"../data/data{size}x{size}en{ensemble}/Temp{temperature}00000/{qty}.csv"
+	data:pd.DataFrame = pd.read_csv(datafile, header=None, usecols=dataIdxRange)
+	return data
+
+def calcMean(data:pd.DataFrame):
+	mean_data = data.mean()
+	mean = mean_data.mean()
+	stDv = mean_data.std()
+	return mean_data, mean, stDv
+
+def plotMeans(data, mean, QtyPerSpin):
+	for i in range(SKIP-1, ROWS, SKIP):
+		plt.plot(data[i], "*", markevery=EVERY, label=f"En {i+1}")
+	plt.plot(mean, "k-")
+	plt.grid()
+	plt.title(f"T={T}, Size={sz}")
+	plt.xlabel("MC steps")
+	plt.ylabel(QtyPerSpin + " per spin")
+	plt.legend()
+
+def vary(p, n):
+	return p[0] / np.sqrt(n) + p[1]
+
+def error(p, n, y):
+	return vary(p, n) - y
+
+# sizeList = np.array([32, 48, 64, 80, 96, 112])
+sizeList = np.array([64])
+stEList  = []
+
+T  = float(2.2)
+EN = 25
+
+for sz in sizeList:
+	eData = readData(T, sz, EN, "energy")
+	# mData = readData(T, sz, EN, "magnet").abs()
+
+	ROWS = len(eData.axes[0])
+	COLS = len(eData.axes[1])
+	print(ROWS, COLS)
+
+	meanE, U, stU = calcMean(eData)
+	# meanM, M, stM = calcMean(mData)
+	stEList.append(stU)
+	print(stU)
+
+	eData = eData.transpose()
+	# mData = mData.transpose()
+
+	plt.figure()
+	plotMeans(eData, meanE, "Energy")
+	print("<E> = ", U, end="\t")
+	print(r"$\Delta E$ = ", stU)
+
+	# plt.figure()
+	# plotMeans(mData, meanM, "Magnetisation")
+	# print("<M> = ", M, end="\t")
+	# print(r"\Delta M = ", stM)
+
+stEList = np.array(stEList)
+
+plt.figure()
+plt.plot(sizeList, stEList, "rx")
+p0 = [1, 1]
+p_fit = opt.least_squares(error, p0[:], args=(sizeList, stEList)).x
+print(p_fit)
+
+N = np.arange(24, 128, 0.1)
+plt.plot(N, vary(p_fit, N))
+plt.grid()
+plt.xlabel("Size of lattice")
+plt.ylabel(r"$\frac{\Delta E}{E}$")
+plt.title(f"T = {T}")
+
+plt.show()
