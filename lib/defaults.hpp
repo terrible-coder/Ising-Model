182d31b0f49d145efb813aeea21293f795548e57
Ayanava De <ayanavade01@gmail.com>
2022-09-11 23:55:05 +0530

    Restoring the default settings header

---
+++ lib/defaults.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <cstdint>
+#include <iostream>
+#include <string>
+
+#define FONTS "./assets/font/times new roman.ttf"
+#define DATA_EXT ".csv"
+#define BIN_EXT ".ism"
+#define IMG_EXT ".png"
+#define DATA_DIR_PREFIX "data/data"
+#define INPUT_FILE "input.txt"
+#define SEPARATOR "\n==========================\n"
+
+#define TEXT_SIZE 16
+#define STAT_BAR_H 20
+#define RUN 15000
+#define SKIP 10
+
+#define WORD_SIZE 64
+typedef std::uint64_t uWord_t;
